17830feccc7341e8c8656797ad78f43d601a2b9f
barbaragribeiro <barbaragomes0@hotmail.com>
2020-04-09 01:38:08 -0300

    Adicionando o arquivo que faltou

---
+++ client/src/components/Informe.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-card :title="postData.title" :header="postData.date">
+    <b-card-text> {{postData.body}} </b-card-text>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: 'Informe',
+  props: ['postData']
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import 'node_modules/bootstrap/scss/bootstrap.scss';
+  
+  .card-text{
+    text-align: justify;
+  }
+
+  .card-header {
+    padding: 0;
+    color: $gray-600;
+  }
+  
+</style>
