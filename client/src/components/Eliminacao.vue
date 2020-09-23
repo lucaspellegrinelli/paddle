8a1bb154abd0d8204c36f03786772e373c289ead
isadoracristina <isadorac2008@hotmail.com>
2020-09-22 22:18:34 -0300

    arquivo eliminação

---
+++ client/src/components/Eliminacao.vue
@@ -0,0 +1,33 @@
+<template>
+	<b-container fluid="lg">
+		<b-row class="text-center">
+			<b-col>1 of 3</b-col>
+			<b-col cols="8">2 of 3 (wider)</b-col>
+			<b-col>3 of 3</b-col>
+		</b-row>
+
+		<b-row class="text-center">
+			<b-col>1 of 3</b-col>
+			<b-col cols="5">2 of 3 (wider)</b-col>
+			<b-col>3 of 3</b-col>
+		</b-row>
+	</b-container>
+
+
+</template>
+
+<script>
+// const axios = require("axios");
+
+// export default {
+//   name: "FormLogin",
+//   data() {
+//   },
+// }
+</script>
+
+<style lang="scss">
+.row > [class^=col-], .row > .col{
+	border: 1px solid #000;
+}
+</style>
